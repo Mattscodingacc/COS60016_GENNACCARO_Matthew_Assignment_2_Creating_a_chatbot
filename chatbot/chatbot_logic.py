2d6faae7f4525b945588186105420f74f3a117ee
Mattscodingacc <88303766+Mattscodingacc@users.noreply.github.com>
2021-12-09 00:17:06 +1100

    Reupload of project

--- chatbot/chatbot_logic.py
+++ chatbot/chatbot_logic.py
@@ -7,19 +7,24 @@ from django.conf import settings
 with open('chatbot/QnA_chatbot.json', 'r') as f:
     qa_data = f.read()
 
+# Store the json data in variable.
 qa_json = json.loads(qa_data)
 
+# Create blank list and store in train variable.
 train = []
 
+# Append questions and answers in json data to train list.
 for k, r in enumerate(qa_json):
     train.append(r['question'])
     train.append(r['answer'])
 
+# Create CHatBot object called Jarvis, assign to chatbot variable.
 chatbot = ChatBot('Jarvis')
+# Use ListTrainer as the trainer for the chatbot.
 trainer = ListTrainer(chatbot)
-
+# Train the chatbot.
 trainer.train(train)
 
-
+# Create talk function to be used. Allows chatbot to respond to question.
 def talk(msg):
     return chatbot.get_response(msg)
